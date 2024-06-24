c3be5bc2cd339f42a3fadda9125f4f78904bd68b
ADMIN <ADMIN@PC230>
2024-06-24 11:20:55 +0700

    Add project files.

---
+++ WebShopPet/Migrations/20240624031456_KhoiTao.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebShopPet.Migrations
+{
+    public partial class KhoiTao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DisplayOrder = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Bull" },
+                    { 2, 2, "Alaska" },
+                    { 3, 3, "Husky" },
+                    { 4, 4, "Chihuahua" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, 1, null, null, "Bull Anh", 300.0 },
+                    { 2, 1, null, null, "Bull Pháp", 350.0 },
+                    { 3, 1, null, null, "American Bully", 400.0 },
+                    { 4, 1, null, null, "American Pit Bull Terrier", 420.0 },
+                    { 5, 2, null, null, "Alaska Sable – trắng", 630.0 },
+                    { 6, 2, null, null, "Alaska màu lông xám – trắng", 750.0 },
+                    { 7, 2, null, null, "Alaska màu lông nâu đỏ – trắng", 820.0 },
+                    { 8, 3, null, null, "Husky màu đen trắng", 950.0 },
+                    { 9, 3, null, null, "Husky màu xám", 1200.0 },
+                    { 10, 3, null, null, "Husky màu trắng tinh khiết ", 1450.0 },
+                    { 11, 3, null, null, "Husky màu đồng hoặc đỏ", 750.0 },
+                    { 12, 4, null, null, "Chihuahua Socola", 1250.0 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ WebShopPet/Models/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace WebShopPet.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { set; get; }
+        public DbSet<Product> Products { set; get; }
+      //  public DbSet<Order> Orders { get; set; }
+       // public DbSet<OrderDetail> OrderDetails { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //seed data to table Categories<>
+            modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Bull", DisplayOrder = 1 },
+            new Category { Id = 2, Name = "Alaska", DisplayOrder = 2 },
+            new Category { Id = 3, Name = "Husky", DisplayOrder = 3 },
+            new Category { Id = 4, Name = "Chihuahua", DisplayOrder = 4});
+            //seed data to table Product<>
+            modelBuilder.Entity<Product>().HasData(
+            new Product { Id = 1, Name = "Bull Anh", Price = 300, CategoryId = 1 },
+            new Product { Id = 2, Name = "Bull Pháp", Price = 350, CategoryId = 1 },
+            new Product { Id = 3, Name = "American Bully", Price = 400, CategoryId = 1 },
+            new Product { Id = 4, Name = "American Pit Bull Terrier", Price = 420, CategoryId = 1 },
+            new Product { Id = 5, Name = "Alaska Sable – trắng", Price = 630, CategoryId = 2 },
+            new Product { Id = 6, Name = "Alaska màu lông xám – trắng", Price = 750, CategoryId = 2 },
+            new Product { Id = 7, Name = "Alaska màu lông nâu đỏ – trắng", Price = 820, CategoryId = 2 },
+            new Product { Id = 8, Name = "Husky màu đen trắng", Price = 950, CategoryId = 3 },
+            new Product { Id = 9, Name = "Husky màu xám", Price = 1200, CategoryId = 3 },
+            new Product { Id = 10, Name = "Husky màu trắng tinh khiết ", Price = 1450, CategoryId = 3 },
+            new Product { Id = 11, Name = "Husky màu đồng hoặc đỏ", Price = 750, CategoryId = 3 },
+            new Product { Id = 12, Name = "Chihuahua Socola", Price = 1250, CategoryId = 4 });
+        }
+    }
+}
